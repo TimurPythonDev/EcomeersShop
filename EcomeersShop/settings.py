842d2d59c039e57fe7d5e50269ff4cdd133482db
TimurPythonDev <timurkarabayevpython@gmail.com>
2022-01-04 13:43:58 +0300

    our

--- EcomeersShop/settings.py
+++ EcomeersShop/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$#hl!$4f1n2e(swj-pk&vbb!la2ak_i$cxv!f+hsd=86x35)9$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['onlinedehqon.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
